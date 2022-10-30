84b20cdd8a133e063c99a04121fe65e0b6883127
Mari <116832047+DudaShadow@users.noreply.github.com>
2022-10-30 18:51:38 -0300

    intervalo ex
    aula e estudos

---
+++ intervalo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    float num;
+
+    scanf("%f", &num);
+
+    if (num > 100 || num < 0)
+    {
+        printf("Fora de intervalo\n");
+    }
+    else if (num <= 25)
+    {
+        printf("Intervalo [0,25]\n");
+    }
+    else if (num <= 50)
+    {
+        printf("Intervalo (25,50]\n");
+    }
+    else if (num <= 75)
+    {
+        printf("Intervalo (25,75]\n");
+    }
+    else if (num <= 100)
+    {
+        printf("Intervalo (75,100]\n");
+    }
+}
